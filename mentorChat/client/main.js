1b9b2f18d24ef5a60861a3a3752c65e6eaf9f459
jifnotgif <david.doo@stonybrook.edu>
2019-02-16 13:53:29 -0500

    fixing ui of login

--- mentorChat/client/main.js
+++ mentorChat/client/main.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-
+import '/imports/startup/client';
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
