c009d78fac8082302a630bd30e94dbed0610b021
weixin <weixin.tan@stonybrook.edu>
2019-02-16 09:11:36 -0500

    added exoress and other funct

--- dbConnection/db.js
+++ dbConnection/db.js
@@ -1,13 +1,26 @@
 var mysql = require('mysql');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var con = mysql.createConnection({
+var mysql_Pool = mysql.createPool({
                                  host: "localhost",
                                  user: "root",
-                                 password: "Tandolphin98"
-                                 database: ""
+                                 password: "Tandolphin98",
+                                 database: "nyuHack"
                                  });
+app.listen(8080,function(){});
 
-con.connect(function(err) {
+app.post('',function(req, res){});
+
+mysql_Pool.getConnection(function(err, connection ) {
             if (err) throw err;
             console.log("Connected!");
             });
+
+function insert(schemeName){}
+
+function createScheme(schemeName){}
+
