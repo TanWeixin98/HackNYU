be342750ca427a0445b647791f648598f14f5494
weixin <weixin.tan@stonybrook.edu>
2019-02-16 21:23:50 -0500

    added dummy test account

--- mentorChat/client/main.js
+++ mentorChat/client/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+
 import './main.html';
 import '/imports/ui/index';
 
@@ -11,6 +12,15 @@ if(Meteor.isClient){
       return !Session.get("firstRun");
     }
   });
+
+Accounts.createUser({username:"wei",password:"tan"});
+Accounts.createUser({username:"wei2",password:"tan"});
+Accounts.createUser({username:"wei1",password:"tan"});
+Accounts.createUser({username:"wei3",password:"tan"});
+Accounts.createUser({username:"wei4",password:"tan"});
+
+  
+
   Template.login.events({
     "submit form": function (e, data, tpl) {
       // check for valid user
